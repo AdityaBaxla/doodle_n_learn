c81bfb5e4ccc620635a6a2a35e860bf6aae8c30c
Aditya Baxla <adityabaxla@gmail.com>
2023-11-28 22:03:33 +0530

    added drawing area

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:signature/signature.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
@@ -8,10 +9,41 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  final SignatureController _controller = SignatureController(
+    penColor: Colors.black,
+    penStrokeWidth: 4,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Game area')),
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 300,
+            height: 200,
+            decoration: BoxDecoration(
+              border: Border.all(),
+            ),
+            child: Signature(
+              controller: _controller,
+              height: 200,
+              backgroundColor: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                //clear drawing
+                _controller.clear();
+              },
+              child: Text('clear'))
+        ],
+      )),
     );
   }
 }
